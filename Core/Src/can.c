268b64f1b36b96794bc8b75d2df4d636da347411
APBashara <alexbashara2002@gmail.com>
2024-10-08 16:39:33 -0500

    Initial CAN Driver

---
+++ Core/Src/can.c
@@ -0,0 +1,33 @@
+/************************************************
+* @file    can.c 
+* @author  APBashara
+* @date    10/2024
+* 
+* @brief   CAN Driver Implementation for STM32F042x6
+***********************************************/
+
+#include "can.h"
+
+CAN_Status CAN_Init() {
+
+}
+
+CAN_Status CAN_Filters_Init() {
+
+}
+
+CAN_Status CAN_Start() {
+
+}
+
+CAN_Status CAN_Stop() {
+
+}
+
+CAN_Status CAN_Transmit(CAN_Frame *frame) {
+
+}
+
+CAN_Status CAN_Receive(CAN_Frame *frame) {
+
+}
