9280f55f24f2d40b396c9706639e25c47ec9eb37
APBashara <alexbashara2002@gmail.com>
2024-10-08 16:26:34 -0500

    Initial Commit

---
+++ Core/Src/main.c
@@ -0,0 +1,13 @@
+/************************************************
+* @file    main.c 
+* @author  APBashara
+* @date    10/2024
+* 
+* @brief   Main Code to run Tasks and Setup Peripherals
+***********************************************/
+
+#include "main.h"
+
+void main() {
+  
+}
